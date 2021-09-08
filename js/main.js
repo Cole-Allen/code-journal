1d0d16639369271bd3497741ec3e55c88ccabf39
Cole Allen <coleallen3333@gmail.com>
2021-09-08 10:19:41 -0700

    Fixing branch issue

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 /* global data */
 /* exported data */
+function createEntry(entry) {
+  var $li = document.createElement('li');
+  var $imgCon = document.createElement('div');
+  var $textCon = document.createElement('div');
+  var $img = document.createElement('img');
+  var $textH = document.createElement('h2');
+  var $textP = document.createElement('p');
+
+  $li.setAttribute('class', 'entry row column-full');
+  $imgCon.setAttribute('class', 'img-container column-half');
+  $textCon.setAttribute('class', 'img-container column-half');
+
+  $img.setAttribute('src', entry.imageURL);
+
+  $textH.textContent = entry.title;
+  $textP.textContent = entry.text;
+
+  $li.appendChild($imgCon);
+  $li.appendChild($textCon);
+  $imgCon.appendChild($img);
+  $textCon.appendChild($textH);
+  $textCon.appendChild($textP);
+
+  return $li;
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+
+});

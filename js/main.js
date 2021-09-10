cf2958dd8cae82feb29af10391d8e1c387420baa
Cole Allen <coleallen3333@gmail.com>
2021-09-10 15:25:46 -0700

    Task 1 complete

--- js/main.js
+++ js/main.js
@@ -95,6 +95,7 @@ function createEntryData(event) {
   entry.photoURL = document.forms[0].photoURL.value;
   entry.notes = document.forms[0].notes.value;
   entry.entryID = data.nextEntryId;
+  entry.date = Date();
   data.nextEntryId++;
   return entry;
 }
